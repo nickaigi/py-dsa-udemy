b5657e5e2b0d597744daa82cd58f9bdf31169000
Nickson Kaigi <nickaigi@gmail.com>
2026-06-04 17:04:22 +0300

    run length algorithm
    soln using the run len algo

--- array_sequences/string_compression.py
+++ array_sequences/string_compression.py
@@ -6,21 +6,39 @@ For instance, it is okay for "AAB" to return "A2B1" even though this technically
 The function should be case sensitive, so that a string "AAAaaa" returns "A3a3"
 """
 
-from collections import defaultdict
 
+def compress(s: str) -> str:
+    """
+    Run length Algorithm
+    """
+    r = ""
+    n = len(s)
 
-def str_compress(s: str) -> str:
-    d = defaultdict(int)
-    for char in s:
-        d[char] += 1
-    return "".join([f"{k}{v}" for k, v in d.items()])
+    if n == 0:  # edge case where string is empty
+        return ""
+    if n == 1:  # edge case where string has 1 letter
+        return s + "1"
+
+    count = 1
+    i = 1
+
+    while i < n:
+        if s[i] == s[i - 1]:
+            count += 1
+        else:
+            r = r + s[i - 1] + str(count)
+            count = 1
+        i += 1
+
+    r = r + s[i - 1] + str(count)
+    return r
 
 
 if __name__ == "__main__":
     s = "AAAABBBBCCCCCDDEEEE"
-    assert str_compress(s) == "A4B4C5D2E4"
+    assert compress(s) == "A4B4C5D2E4"
     s = "AAB"
-    assert str_compress(s) == "A2B1"
+    assert compress(s) == "A2B1"
     s = "AAAaaa"
-    assert str_compress(s) == "A3a3"
+    assert compress(s) == "A3a3"
     print("All Tests Passed")
